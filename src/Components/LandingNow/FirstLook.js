2f0c47d2d7416a926cf6ba8fb42f4bf36b307542
raviraj3214 <raviraj32140@gmail.com>
2023-10-17 12:18:41 +0530

    landing page resp

--- src/Components/LandingNow/FirstLook.js
+++ src/Components/LandingNow/FirstLook.js
@@ -3,17 +3,17 @@ import CommingSoonImg from "../../images/Buyer-Landing-page.png"
 
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Helmet } from 'react-helmet'; 
+import { Helmet } from 'react-helmet';
 function FirstLook() {
-  
+
   const [email, setEmail] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-      // Submit the form data to backend API here
-  
-   
+
+    // Submit the form data to backend API here
+
+
     try {
       const response = await axios.post('https://api.seventhsq.com/subscribe/', { email });
       console.log(response.data);
@@ -22,34 +22,32 @@ function FirstLook() {
       console.error(error);
       // TODO: Handle error
     }
-  
-  setIsSubmitted(true);
-  setEmail('');
-};
+
+    setIsSubmitted(true);
+    setEmail('');
+  };
 
 
   function handleButtonClick() {
     window.open('https://seller.seventhsq.com', '_blank');
-    }
+  }
   return (
-    <div style={{ backgroundColor:"#F8F8F8"}}>
-      
-    <Helmet>
-    <title>Seventh Square | E-Commerce Marketplace for Building Materials & Home Improvement products in India</title>
-    {/* The best website for buying Construction material */}
-    <meta name="description" content="Shop for Building Material & Home Improvement Products online with us!" />
-  </Helmet>
-      <div className="container row ml-5 "  >
-        <div className="col-md-6">
-          <img className="mt-5 ml-5"
-            style={{ width: "83%" }}
-            src={CommingSoonImg}>
-          </img>
+    <div className="container " style={{ backgroundColor: "#F8F8F8" }}>
+
+      <Helmet>
+        <title>Seventh Square | E-Commerce Marketplace for Building Materials & Home Improvement products in India</title>
+        {/* The best website for buying Construction material */}
+        <meta name="description" content="Shop for Building Material & Home Improvement Products online with us!" />
+      </Helmet>
+      <div className="row  col-lg-12 col-12"  >
+        <div className="col-md-6 col-lg-6">
+          <img className="mt-5 ml-5" style={{ width: "83%" }} src={CommingSoonImg} alt="A coming soon graphic" />
         </div>
 
+
         <div
-          className="col-md-6"
-          style={{ justifyContent: "center", marginTop: "140px" }}
+          className="col-md-6 col-lg-6 pr-lg-0 mr-lg-0 "
+          style={{ marginTop: "140px" }}
         >
           <h1
             style={{
@@ -73,7 +71,7 @@ function FirstLook() {
           </p>
         </div>
       </div>
-      <div className="row col-md-12" style={{ backgroundColor:"#F8F8F8"}}>
+      <div className="row col-md-12" style={{ backgroundColor: "#F8F8F8" }}>
         <h1
           style={{
             justifyContent: "center",
@@ -96,74 +94,76 @@ function FirstLook() {
           exciting launch offers!
         </h1>
 
-     
-      <form onSubmit={handleSubmit}>
-      <div className="row">
-        <input
-          type="email"
-          class="form-control"
-          value={email}
-          name={email}
-          placeholder="Enter Email"
-          aria-label="Search Your Product"
-          aria-describedby="button-addon2"
-          onChange={(e) => setEmail(e.target.value)}
-          
+
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <input
+              type="email"
+              class="form-control"
+              value={email}
+              name={email}
+              placeholder="Enter Email"
+              aria-label="Search Your Product"
+              aria-describedby="button-addon2"
+              onChange={(e) => setEmail(e.target.value)}
+
+              style={{
+                width: "35%",
+                justifyContent: "center",
+                display: "flex",
+                textAlign: "justify",
+                marginLeft: "28%",
+                marginTop: "11px",
+                fontSize: "16px",
+                fontFamily: "Arial",
+              }}
+            />
+            <button
+              className="btn btn-dark"
+              type="submit"
+              style={{
+                width: "143px",
+                height: "34px",
+                marginTop: "10px",
+                fontSize: "16px",
+                fontFamily: "Arial",
+                textTransform: "none"
+              }}
+            >
+              Get Notified!
+            </button>
+
+          </div>
+        </form>
+        <div style={{
+
+          marginTop: "10px",
+          fontSize: "16px",
+          fontFamily: "Arial",
+          textTransform: "none"
+        }}>
+          {isSubmitted && (
+            <p style={{
+              color: "green",
+              marginLeft: "500px",
+            }}>Thank you for submitting the form!</p>
+          )}
+        </div>
+        <h3
           style={{
-            width: "35%",
             justifyContent: "center",
             display: "flex",
             textAlign: "justify",
-            marginLeft: "28%",
-            marginTop: "11px",
-            fontSize: "16px",
+            marginTop: "30px",
             fontFamily: "Arial",
-          }}
-        />
-      <button
-          className="btn btn-dark"
-          type="submit"
-          style={{
-            width: "143px",
-            height: "34px",
-            marginTop: "10px",
-            fontSize: "16px",
-            fontFamily: "Arial",
-            textTransform: "none" 
           }}
         >
-          Get Notified!
-        </button>
+          Selling Building Materials ? Sell with us here :
+          <span onClick={handleButtonClick} style={{ backgroundColor: "#F8F8F8", color: "black", marginTop: "-5px", border: "none", padding: "5px 10px", borderRadius: "5px", textDecoration: "none", cursor: "pointer" }}>
+            seller.seventhsq.com
+          </span>
+        </h3>
 
-      </div>
-    </form>
-    <div  style={{
-     
-      marginTop: "10px",
-      fontSize: "16px",
-      fontFamily: "Arial",
-      textTransform: "none" 
-    }}>
-    {isSubmitted && (
-      <p style={{ color: "green",
-      marginLeft:"500px", }}>Thank you for submitting the form!</p>
-    )}
-    </div>
-    <h3
-    style={{
-      justifyContent: "center",
-      display: "flex",
-      textAlign: "justify",
-      marginTop: "30px",
-      fontFamily: "Arial",
-    }}
-  >
-    Selling Building Materials ? Sell with us here :
-    <span onClick={handleButtonClick} style={{ backgroundColor: "#F8F8F8", color: "black",marginTop:"-5px", border: "none", padding: "5px 10px", borderRadius: "5px", textDecoration: "none", cursor: "pointer" }}>
-      seller.seventhsq.com
-    </span>
-  </h3>
-  
       </div>
     </div>
   );
